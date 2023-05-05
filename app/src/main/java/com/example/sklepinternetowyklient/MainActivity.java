816f1927795050131ae05db692460f6697bafbc6
Matix351 <130908742+Matix351@users.noreply.github.com>
2023-05-05 21:52:49 +0200

    CreateProject

---
+++ app/src/main/java/com/example/sklepinternetowyklient/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.sklepinternetowyklient;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            Intent nextActivity = new Intent(MainActivity.this, MainActivity2ShopView.class);
+
+
+
+            @Override
+            public void run() {
+
+                MainActivity.this.startActivity(nextActivity);
+
+            }
+        };
+        timer.schedule(task, 1500);
+
+
+
+        //Intent nextActivity = new Intent(MainActivity.this, MMmNFOptiActivity.class);
+        //Intent nextActivity = new Intent(MainActivity.this, MainActivity2.class);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/sklepinternetowyklient/MainActivity2ShopView.java
@@ -0,0 +1,14 @@
+package com.example.sklepinternetowyklient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2ShopView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_shop_view);
+    }
+}
